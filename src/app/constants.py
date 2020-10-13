9d5d3a48713c195b2ea870bd3cc644904d0decbf
Hernan DT <hernan.ditota@gentem.com>
2020-10-12 23:08:22 -0300

    update files folder

--- src/app/constants.py
+++ src/app/constants.py
@@ -3,8 +3,8 @@ SERVER_HOST = '127.0.0.1'
 SERVER_PORT = 8080
 
 #DATABASE and Medicare data Path
-DATABASE_NAME = '/usr/src/files/science.db'
-FILE_NAME = '/usr/src/files/medicare_data.csv'
+DATABASE_NAME = '/usr/files/science.db'
+FILE_NAME = '/usr/files/medicare_data.csv'
 
 #RBQL confing
 FILE_PATH_INPUT = 'bounded_data.csv'
